99f05677caa7e4391d586ebdd61d9028e03161dc
Peter Novotnak <peter@cyrusbio.com>
2015-12-22 17:35:04 -0800

    bumped version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='dj_bioinformatics_protein',
-      version='0.0.1',
+      version='0.0.2',
       description='Django utilities and tools for protein storage and manipulation',
       url='http://github.com/CyrusBiotechnology/dj-protein',
       author='Peter Novotnak, Yifan Song',
