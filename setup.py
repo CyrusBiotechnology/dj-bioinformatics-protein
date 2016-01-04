dce6a91fab8e5301a94440e49d77e2c49a35946d
Peter Novotnak <peter@cyrusbio.com>
2016-01-04 14:32:24 -0800

    added initial migration

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='dj_bioinformatics_protein',
-      version='0.0.2',
+      version='0.0.3',
       description='Django utilities and tools for protein storage and manipulation',
       url='http://github.com/CyrusBiotechnology/dj-protein',
       author='Peter Novotnak, Yifan Song',
       author_email='peter@cyrusbio.com, yifan@cyrusbio.com',
       license='MIT',
-      packages=['dj_bioinformatics_protein'],
+      packages=['dj_bioinformatics_protein', 'dj_bioinformatics_protein.migrations'],
       zip_safe=True)
